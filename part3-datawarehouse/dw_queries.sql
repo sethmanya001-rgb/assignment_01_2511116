7e7f23930a9c557b1a3204a273793242395f9b16
sethmanya001-rgb <sethmanya001@gmail.com>
2026-03-21 18:09:12 +0530

    Add SQL queries for sales analysis
    Added SQL queries for total sales revenue, top stores, and sales trend.

---
+++ part3-datawarehouse/dw_queries.sql
@@ -0,0 +1,39 @@
+-- Q1: Total sales revenue by product category for each month
+
+SELECT 
+dim_date.month,
+dim_product.category,
+SUM(fact_sales.total_amount) AS total_revenue
+FROM fact_sales
+JOIN dim_date 
+ON fact_sales.date_id = dim_date.date_id
+JOIN dim_product 
+ON fact_sales.product_id = dim_product.product_id
+GROUP BY dim_date.month, dim_product.category;
+
+
+
+-- Q2: Top 2 performing stores by total revenue
+
+SELECT 
+dim_store.store_name,
+SUM(fact_sales.total_amount) AS total_revenue
+FROM fact_sales
+JOIN dim_store
+ON fact_sales.store_id = dim_store.store_id
+GROUP BY dim_store.store_name
+ORDER BY total_revenue DESC
+LIMIT 2;
+
+
+
+-- Q3: Month-over-month sales trend across all stores
+
+SELECT 
+dim_date.month,
+SUM(fact_sales.total_amount) AS monthly_sales
+FROM fact_sales
+JOIN dim_date
+ON fact_sales.date_id = dim_date.date_id
+GROUP BY dim_date.month
+ORDER BY dim_date.month;
